5d38d61747aef7143187b24204a2caef379f9a6e
pakelcomedy <releaseonegit@gmail.com>
2026-01-22 12:17:49 +0700

    Delete

--- simulated_github_repos/repo_022/scripts/load_dataset.py
+++
@@ -1,9 +0,0 @@
-
-import torch
-import tensorflow as tf
-class ModelLoader:
-    def __init__(self, model_dir: str):
-        self.model_dir = Path(model_dir)
-    def load_pytorch_model(self, model_path: str):
-        return torch.load(model_path, map_location='cpu')
-            
